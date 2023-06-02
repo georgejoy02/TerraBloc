6d4b6ad465f4ca1dc527a0772f4c3ceb96dac7ac
xntrik <georgepjoy16@gmail.com>
2023-06-03 03:21:11 +0530

    addlandinpextor,getweb3 changed

--- server/web3/getweb3.js
+++ server/web3/getweb3.js
@@ -7,7 +7,7 @@ const web3 = new Web3(rpcURL)
 const web3Init = async () => {
     const net_id = await web3.eth.net.getId();
     const deployedNetwork = contract.networks[net_id]
-    const land_contract = await new web3.eth.Contract(contract.abi, deployedNetwork.address)
+    const land_contract = await new web3.eth.Contract(contract.abi, deployedNetwork.address, { handleRevert: true })
     return land_contract
 }
 module.exports = web3Init
