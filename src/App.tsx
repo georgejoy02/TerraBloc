9a3604a1ed4b71dbcc96609c97ba49c2406f7d2a
rebin03 <muhammedrebin1794@gmail.com>
2023-06-04 23:03:07 +0530

    Removed Comment in App.js

--- src/App.tsx
+++ src/App.tsx
@@ -30,12 +30,8 @@ export default function App() {
           <Route path="/loginoption" element={<LoginPage />} />
           <Route path="/admin" element={<Admin />} />
           <Route path="/registeruser" element={<RegisterUser />} />
-          
         </Routes>
       </BrowserRouter>
-      {/* <ConfirmPaymentPopup open={true} onClose={function (): void {
-        throw new Error("Function not implemented.");
-      } } userAddress1={"5y8d75yd8437mxtm3387436473453c4t3847ty8374tc34t7"} userAddress2={"wciurown3i4u837ywehf27487382725728782kfjnkw4d5"} totalAmountInRupees={"50000"} ethValue={"329172.34"} totalAmountInEth={"1.52342456"} /> */}
     </div>
   )
 }
