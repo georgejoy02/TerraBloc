104642080961d846113204d600b57a6211be8005
xntrik <georgepjoy16@gmail.com>
2023-06-10 01:07:56 +0530

    initial migration removed,ganache auto run added

--- smartcontract/migrations/1_deploy_migration.js
+++ smartcontract/migrations/1_deploy_migration.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-undef
 const landContract = artifacts.require("Land");
 
-module.exports = (deployer)=> {
+module.exports = (deployer) => {
   deployer.deploy(landContract);
 };
