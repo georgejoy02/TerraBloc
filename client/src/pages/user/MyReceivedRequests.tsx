9630c2c56c1a4641612f373bb3aa0f7b82208714
Muhammed Risin <muhammedrisin999@gmail.com>
2023-06-17 03:22:42 +0530

    Updated UserDashboard

--- client/src/pages/user/MyReceivedRequests.tsx
+++ client/src/pages/user/MyReceivedRequests.tsx
@@ -1,11 +1,97 @@
-import React from 'react'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
 
-const MyReceivedRequests = () => {
-    return (
-        <div>
-            <h1>MyReceivedRequests</h1>
-        </div>
-    )
+function createData(
+    si: number,
+    landId: string,
+    buyerAddress: string,
+    status: string,
+    paymentDone: string,
+    reject: boolean,
+    accept: boolean
+) {
+    return { si, landId, buyerAddress, status, paymentDone, reject, accept };
 }
 
-export default MyReceivedRequests
+const rows = [
+    createData(
+        1,
+        "8983924938",
+        "Prayikkalam",
+        "no status",
+        "done",
+        false, //rejecct
+        false), //accept
+];
+
+const MyReceivedRequests = () => {
+    const handleAccept = (row: any) => {
+        // Perform accept action
+        console.log("Accept clicked for row:", row);
+    };
+
+    const handleReject = (row: any) => {
+        // Perform reject action
+        console.log("Reject clicked for row:", row);
+    };
+
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>#</TableCell>
+                        <TableCell>Land Id</TableCell>
+                        <TableCell>Buyer Address</TableCell>
+                        <TableCell>Status</TableCell>
+                        <TableCell>Payment Done</TableCell>
+                        <TableCell>Reject</TableCell>
+                        <TableCell>Accept</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map((row) => (
+                        <TableRow
+                            key={row.landId}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell>{row.si}</TableCell>
+                            <TableCell>{row.landId}</TableCell>
+                            <TableCell>{row.buyerAddress}</TableCell>
+                            <TableCell>{row.status}</TableCell>
+                            <TableCell>{row.paymentDone}</TableCell>
+                            <TableCell>
+                                <Button
+                                    variant="contained"
+                                    color="error"
+                                    disabled={row.reject || row.accept}
+                                    onClick={() => handleReject(row)}
+                                >
+                                    Reject
+                                </Button>
+                            </TableCell>
+                            <TableCell>
+                                <Button
+                                    variant="contained"
+                                    color="success"
+                                    disabled={row.reject || row.accept}
+                                    onClick={() => handleAccept(row)}
+                                >
+                                    Accept
+                                </Button>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+};
+
+export default MyReceivedRequests;
--- client/src/pages/user/MySentLandRequests.tsx
+++ client/src/pages/user/MySentLandRequests.tsx
@@ -1,11 +1,72 @@
-import React from 'react'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
 
-const MySentLandRequests = () => {
-    return (
-        <div>
-            <h1>MySentLandRequests</h1>
-        </div>
-    )
+function createData(
+    si: number,
+    landId: string,
+    ownerAddress: string,
+    status: string,
+    price: string
+) {
+    return { si, landId, ownerAddress, status, price };
 }
 
-export default MySentLandRequests
+const rows = [
+    createData(1, "890381283498092832", "Owner's Address 1", "Pending", "₹50000"),
+    createData(2, "783920183409283047", "Owner's Address 2", "Completed", "₹75000"),
+];
+
+const MySentLandRequests = () => {
+    const handleMakePayment = (row: any) => {
+        // Perform make payment action
+        console.log("Make Payment clicked for row:", row);
+    };
+
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>#</TableCell>
+                        <TableCell>Land Id</TableCell>
+                        <TableCell>Owner Address</TableCell>
+                        <TableCell>Status</TableCell>
+                        <TableCell>Price (in ₹)</TableCell>
+                        <TableCell>Make Payment</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map((row) => (
+                        <TableRow
+                            key={row.landId}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell>{row.si}</TableCell>
+                            <TableCell>{row.landId}</TableCell>
+                            <TableCell>{row.ownerAddress}</TableCell>
+                            <TableCell>{row.status}</TableCell>
+                            <TableCell>{row.price}</TableCell>
+                            <TableCell>
+                                <Button
+                                    variant="contained"
+                                    disabled={row.status === "Completed"}
+                                    onClick={() => handleMakePayment(row)}
+                                >
+                                    Make Payment
+                                </Button>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+};
+
+export default MySentLandRequests;
