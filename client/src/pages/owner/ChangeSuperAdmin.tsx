f00dfb9b09021ad332a0a65504c46c8f567e0f94
Muhammed Risin <muhammedrisin999@gmail.com>
2023-06-18 17:18:31 +0530

    updates in owner dashboard

--- client/src/pages/owner/ChangeSuperAdmin.tsx
+++ client/src/pages/owner/ChangeSuperAdmin.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Box, Typography, TextField, Button } from '@mui/material';
 
-function ChangeSuperAdmin() {
+const ChangeSuperAdmin = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    // Display confirm pop-up
+    const confirmMessage = 'Are you sure you want to change the Owner?';
+    const isConfirmed = window.confirm(confirmMessage);
+
+    if (isConfirmed) {
+      // Handle form submission logic here
+      console.log('Submitted value:', inputValue);
+    }
+  };
+
   return (
-    <div>ChangeSuperAdmin</div>
-  )
-}
+    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-start', height: '100vh', paddingTop: '200px' }}>
+      <Box component="form" sx={{ p: 2, display: 'flex', flexDirection: 'column', alignItems: 'center' }} onSubmit={handleSubmit}>
+        <Typography variant="h4" mb={3}>
+          Change Super Admin
+        </Typography>
+        <TextField
+          required
+          type="text"
+          label="New Super Admin"
+          sx={{ width: '700px', mb: 2 }}
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <Button type="submit" variant="contained" color="primary">
+          Submit
+        </Button>
+      </Box>
+    </Box>
+  );
+};
 
-export default ChangeSuperAdmin
+export default ChangeSuperAdmin;
